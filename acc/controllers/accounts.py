6a241958411660466a2489c7dc1f033cfcc3e136
Lucas Dantas Gueiros <lucas.dgueiros@ufrpe.br>
2022-09-21 20:13:10 +0000

    calculando saldo final das contas

--- acc/controllers/accounts.py
+++ acc/controllers/accounts.py
@@ -1,6 +1,25 @@
 from cement import Controller, ex, fs
 import csv
-from anytree import Node, RenderTree
+from anytree import Node, RenderTree, NodeMixin
+from decimal import Decimal
+
+class Account(NodeMixin):
+    def __init__(self, name, saldo = Decimal(0), parent=None, children=None):
+        super(Account,self).__init__()
+        self.name = name
+        self.saldo = saldo
+        self.parent = parent
+        if children:
+            self.children = children
+
+    def sum_saldo(self, valor):
+        self.saldo = self.saldo + valor
+
+    def saldo_subcontas(self):
+        saldo_subcontas = self.saldo
+        for child in self.children:
+            saldo_subcontas = saldo_subcontas + child.saldo_subcontas() 
+        return saldo_subcontas
 
 class Accounts(Controller):
     class Meta:
@@ -16,33 +35,37 @@ class Accounts(Controller):
             )
     def list(self):
         fs.backup(self.app.pargs.input_file)
-        radix = Node('Radix')
+        radix = Account('Radix')
         accounts = {'radix': radix}
         with open(self.app.pargs.input_file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
+            
+            account_column = 1
+            amount_column = 2
+            first_row = True
             for row in csv_reader:
-                if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
-                    line_count += 1
-                else:
-                    complete_name = row[4]
-                    path = complete_name.split(':')
-                    last= radix
-                    name = ''
-                    for part_name in path:
-                        if name == '':
-                            name = part_name
-                        else:
-                            name = ':'.join([name,part_name])
+                if first_row:
+                    account_column = row.index("Full Account Name")
+                    amount_column = row.index("Amount Num.")
+                    first_row = False
+                    continue
+                complete_name = row[account_column]
+                path = complete_name.split(':')
+                last= radix
+                name = ''
+                for part_name in path:
+                    if name == '':
+                        name = part_name
+                    else:
+                        name = ':'.join([name,part_name])
 
-                        if not (name in accounts):
-                            node = Node(name,parent=last)
-                            accounts[name] = node
-                            last = node
-                        else:
-                            last = accounts[name]
-                    line_count += 1
-            print(f'Processed {line_count} lines.')
+                    if not (name in accounts):
+                        node = Account(name, parent=last)
+                        accounts[name] = node
+                        last = node
+                    else:
+                        last = accounts[name]
+                valor = Decimal(row[amount_column].replace(',','.'))
+                last.sum_saldo(valor)
         for pre, fill, node in RenderTree(radix):
-            print("%s%s" % (pre, node.name))
+            print("%s%s\t%s" % (pre, node.name, node.saldo))
